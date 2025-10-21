d1b30e46b4f635c6566d6600f56875e0dc762d0e
Per Olsen <persteen22@gmail.com>
2025-10-21 16:34:09 +0200

    Trying again

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,10 +83,10 @@ TEMPLATES = [
          # Initially create the dir 'templates' at root level
          # Root level is fine because there is only one Django App in the Project
          # 'DIRS': [],
-         #'DIRS': [BASE_DIR / 'templates'],
+         'DIRS': [BASE_DIR / 'templates'],
 
          # 21-10-2025 - Test for Azure
-         'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
+        # 'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         
 
         'APP_DIRS': True,
@@ -181,13 +181,18 @@ USE_TZ = True
 
 # 21-10-2025 - Testing for Azure
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+
+# 21-10-2025 - Works locally
 # STATICFILES_DIRS = (str(BASE_DIR.joinpath('assets')),)
+
 STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # 21-10-2025 - Testing for Azure
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 21-10-2025 - Works locally
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
